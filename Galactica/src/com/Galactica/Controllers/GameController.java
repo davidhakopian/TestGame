fe04e1f3b52873618d123d07f31bd6a8e979fe4f
davidhakopian <david.hakopian@mail.mcgill.ca>
2014-11-19 21:00:44 -0500

    added CMV

---
+++ Galactica/src/com/Galactica/Controllers/GameController.java
@@ -0,0 +1,24 @@
+package com.Galactica.Controllers;
+
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import com.Galactica.Views.Field;
+
+public class GameController {
+	
+	protected Field mField;
+	
+	public GameController()
+	{
+		//create Field with paint functin defined in controller
+		mField = new Field(ka){
+			@Override
+			public void paintComponent(Graphics g){
+				super.paintComponent(g);
+		        Toolkit.getDefaultToolkit().sync();
+			}
+		};
+	}
+
+}
---
+++ Galactica/src/com/Galactica/Views/Application.java
@@ -0,0 +1,37 @@
+package com.Galactica.Views;
+
+import javax.swing.JFrame;
+
+//import Controllers.GameController;
+
+public class Application extends JFrame {
+
+	// constants
+	public static final int SCREEN_WIDTH = 400;
+	public static final int SCREEN_HEIGHT = 600;
+	public static final String APP_NAME = "Galactica";
+	
+	public static final int TIMEOUT = 20;
+	
+	
+	//protected GameController controller = new GameController();
+	
+	public  Application(){
+		init();
+	}
+	
+	/**
+	 * Initialize the window
+	 */
+	private void init(){
+		//add(controller.getField());
+		setSize(SCREEN_WIDTH,SCREEN_HEIGHT);	
+		setTitle(APP_NAME);
+		//pack();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+}
+
